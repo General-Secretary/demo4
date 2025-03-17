d56faa0f2f8272a02bc20db91946ff8dceedfef2
General-Secretary <omar1313zedan@gmail.com>
2025-03-17 13:55:50 +0200

    إصلاح الأخطاء وتحسين الأداء

--- src/components/Home Comp/Describe.jsx
+++ src/components/Home Comp/Describe.jsx
@@ -17,28 +17,28 @@ export default function Describe({ marginTop }) {
                 <Link href={"/charity"} className='w-full xs:w-[50%] lg:w-[25%] md:p-5 lg:p-7 my-5'>
                     <div className=" w-[80%] mx-auto h-[300px]  bg-gray-700 rounded-[40px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 ">
                         <div className=' relative transform scale-125 md:scale-150 hover:scale-[1.75] transition-transform duration-300  z-10'>
-                            <Image unoptimized src={"/assits/9b2418014ec8bfa6c4453f26c54e57c8.PNG"} width={400} height={400} alt="" className="relative  z-10" />
+                            <Image unoptimized src={"/assits/9b2418014ec8bfa6c4453f26c54e57c8.png"} width={400} height={400} alt="" className="relative  z-10" />
                         </div><h4 className='text-center  lg:mt-10 font-vazir text-white  font-extrabold text-2xl'>مؤسسه خيريه</h4>
                     </div>
                 </Link>
                 <Link href={"/IncubatorSignUp"} className='w-full xs:w-[50%] lg:w-[25%] md:p-5 lg:p-7 my-5'>
                     <div className=" w-[80%] mx-auto h-[300px]   bg-gray-700 rounded-[40px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 ">
                         <div className=' relative transform scale-125 md:scale-150 hover:scale-[1.75] transition-transform duration-300  z-10'>
-                            <Image unoptimized src={"/assits/a987f0341e3156c51ebd63296773d265.PNG"} width={400} height={400} alt="" className="relative  z-10" />
+                            <Image unoptimized src={"/assits/a987f0341e3156c51ebd63296773d265.png"} width={400} height={400} alt="" className="relative  z-10" />
                         </div>  <h4 className='text-center lg:mt-10  font-vazir text-white  font-extrabold text-2xl'>منظمه داعمه</h4>
                     </div>
                 </Link>
                 <Link href={"/InvestorSignUp"} className='w-full xs:w-[50%] lg:w-[25%] md:p-5 lg:p-7 my-5'>
                     <div className=" w-[80%] mx-auto h-[300px]   bg-gray-700 rounded-[40px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 ">
                         <div className=' relative transform scale-125 md:scale-150 hover:scale-[1.75] transition-transform duration-300  z-10'>
-                            <Image unoptimized src={"/assits/ca1e1cd98ef8a7072e82ad0683e4de45.PNG"} width={400} height={400} alt="" className="relative  z-10" />
+                            <Image unoptimized src={"/assits/ca1e1cd98ef8a7072e82ad0683e4de45.png"} width={400} height={400} alt="" className="relative  z-10" />
                         </div> <h4 className='text-center lg:mt-10  font-vazir text-white  font-extrabold text-2xl'>مستثمر</h4>
                     </div>
                 </Link>
                 <Link href={"/startUpSignUp"} className='w-full xs:w-[50%] lg:w-[25%] md:p-5 lg:p-7 my-5'>
                     <div className=" w-[80%] mx-auto h-[300px]   bg-gray-700 rounded-[40px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 ">
                         <div className=' relative transform scale-125 md:scale-150 hover:scale-[1.75] transition-transform duration-300  z-10'>
-                            <Image unoptimized src={"/assits/33b79edb64b2778583faff1b90e8cc51.PNG"} width={400} height={400} alt="" className="relative  z-10" />
+                            <Image unoptimized src={"/assits/33b79edb64b2778583faff1b90e8cc51.png"} width={400} height={400} alt="" className="relative  z-10" />
                         </div><h4 className='text-center lg:mt-10  font-vazir text-white  font-extrabold text-2xl'>شركة ناشئه</h4>
                     </div>
                 </Link>
